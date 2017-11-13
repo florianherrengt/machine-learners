cd08b6277e175bfdcd68568a5827706923e4d18b
Florian HERRENGT <florian.herrengt@gmail.com>
2017-11-13 19:46:41 +0000

    run crawler from cli

--- crawler.py
+++ crawler.py
@@ -1,15 +1,17 @@
 import praw
+import sys
 import numpy as np
 import json
 import logging
 import os
+from tqdm import tqdm
 
 
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-logger = logging.getLogger('prawcore')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
+# handler = logging.StreamHandler()
+# handler.setLevel(logging.DEBUG)
+# logger = logging.getLogger('prawcore')
+# logger.setLevel(logging.DEBUG)
+# logger.addHandler(handler)
 
 reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'],
                      client_secret=os.environ['REDDIT_CLIENT_SECRET'],
@@ -32,6 +34,7 @@ def get_data(sub_name, limit=100):
                 contributions.append({
                     'username': author.name,
                     'subs': list(set([comment.subreddit.display_name for comment in author.comments.new()]))
+                    # 'threads': list(set([comment. for comment in author.comments.new()]))
                 })
             except:
                 pass
@@ -43,6 +46,15 @@ def get_data(sub_name, limit=100):
 
     return results
 
-# def save_to_file(path = 'output.json', results):
-#     with open(path, 'w') as file:
-#         json.dump(results, file)
+def save_to_file(results, path='output.json'):
+    with open(path, 'w') as file:
+        json.dump(results, file)
+
+if __name__ == '__main__':
+    if (len(sys.argv) < 2):
+        raise Exception('You need to pass a sub as first argument')
+    sub = sys.argv[1]
+    if (sub is None):
+        raise Exception('You need to pass a sub as first argument')
+
+    save_to_file(get_data(sub))
